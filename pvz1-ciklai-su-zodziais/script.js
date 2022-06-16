fabc86ab3413731a0a830ade92ef2ffff13e01e1
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-06-16 13:02:56 +0300

    Pabaigtas pirmas pavyzdys su funkcijomis: bruksniuku isvedimas, isvesti zodi po raide, zodzio apsukimas ir zodzio sulipinimas is sonu

--- pvz1-ciklai-su-zodziais/script.js
+++ pvz1-ciklai-su-zodziais/script.js
@@ -1,3 +1,62 @@
 window.onload = function() {
+    let kazkoksZodis = "turnyras";
+
+    isvestiZodiPoRaide(kazkoksZodis);
+    isvestiBruksniukus();
+    isvestiZodiPoRaide("kamuolys");
+    isvestiBruksniukus();
+    console.log(apsukaZodi(kazkoksZodis));
+    isvestiBruksniukus();
+    console.log(apsukaZodi("baravykas"));
+    isvestiBruksniukus();
     
+    
+    // Konvertuoju skaičių į tekstinį kintamąjį, jį apsuka, tada konvertuoju atgal į skaičių ir tą apsuktą skaičių panaudoju skaičiavimuose
+    let x = 2147;
+    x = x.toString();
+    x = apsukaZodi(x);
+    x = Number.parseInt(x);
+
+    let suma = x + 1000;
+    console.log(suma);
+
+    // Dar vienas pavyzdys su žodžio sulipinimu iš šonų
+    isvestiBruksniukus();
+    console.log(sulipinaZodiIsSonu("tvoros"));
+
+    
+
+}
+
+
+function isvestiBruksniukus() {
+    console.log("------------------");
+}
+
+
+function isvestiZodiPoRaide(zodis) {
+    for (let i = 0; i < zodis.length; i++) {
+        console.log(zodis[i]);
+    }
+}
+
+
+
+function apsukaZodi(zodis) {
+    let naujasZodis = "";
+    for (let i = zodis.length - 1; i >= 0; i--) {
+        naujasZodis += zodis[i];
+    }
+    return naujasZodis
+}
+
+
+
+function sulipinaZodiIsSonu(zodis) {
+    let naujasZodis = "";
+    for (let i = 0; i < zodis.length / 2; i++) {
+        naujasZodis += zodis[i];
+        naujasZodis += zodis[zodis.length - 1 - i];
+    }
+    return naujasZodis;
 }
